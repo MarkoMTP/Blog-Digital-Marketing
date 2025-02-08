7ad0a600175ebd089acf798be79ada80f8bc85ec
MarkoMTP <marko.mtp8@gmail.com>
2025-02-08 17:20:49 +0100

    feat: Implement login controller and JWT passport authentication
    - Created login controller to process user credentials, check email/password, and store user data in the database.
    - Implemented JWT passport strategy for secure authentication and route protection.

--- src/passport/passport.js
+++ src/passport/passport.js
@@ -1,2 +1,29 @@
 const { PrismaClient } = require("@prisma/client");
-const passport = require;
+const prisma = new PrismaClient();
+
+const passport = require("passport");
+
+let JwtStrategy = require("passport-jwt").Strategy,
+  ExtractJwt = require("passport-jwt").ExtractJwt;
+
+let options = {};
+options.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
+options.secretOrKey = process.env.JWT_SECRET;
+passport.use(
+  new JwtStrategy(options, async (jwtPayload, done) => {
+    try {
+      const user = await prisma.user.findUnique({
+        where: { id: jwtPayload.id },
+      });
+      if (user) {
+        return done(null, user);
+      } else {
+        return done(null, false);
+      }
+    } catch (err) {
+      return done(err, false);
+    }
+  })
+);
+
+module.exports = passport;
--- src/routes/loginRoutes.js
+++ src/routes/loginRoutes.js
@@ -1,14 +1,54 @@
 const express = require("express");
 const router = express.Router();
-
+const passport = require("../passport/passport");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 //reg validator
 const { registerValidator } = require("../middleware/registerValidator");
 const registerController = require("../controllers/registerController");
+const { findUserEmail } = require("../db/queries");
 
 router.get("/", (req, res) => {
   res.send("HomePage");
 });
 
+// router.get("/register")
+
 router.post("/register", registerValidator, registerController);
+
+// login get
+
+router.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    const user = await findUserEmail(email);
+    if (!user) {
+      return res.status(200).send("User does not exist");
+    }
+
+    if (!(await bcrypt.compare(password, user.password))) {
+      return res.status(200).send("Password is not correct");
+    }
+
+    //create a JWT token
+    const token = jwt.sign(
+      { id: user.id, isAuthor: user.isAuthor },
+      process.env.JWT_SECRET,
+      { expiresIn: "8h" }
+    );
+    res.send({ message: "Logged in", token });
+  } catch (err) {
+    return console.error(err);
+  }
+});
+
+router.get(
+  "/protected",
+  passport.authenticate("jwt", { session: false }), // Use passport middleware to check the token
+  (req, res) => {
+    res.json({ message: "You are authenticated", user: req.user });
+  }
+);
 
 module.exports = router;
